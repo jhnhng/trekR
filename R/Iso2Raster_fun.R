cc26c213fbf77f17b78fe8e5f31f470e3abdd83c
John Huang <john.huang5595@gmail.com>
2021-09-15 14:12:09 -0600

    Update

--- R/Iso2Raster_fun.R
+++ R/Iso2Raster_fun.R
@@ -5,12 +5,11 @@
 #'
 #' @return The output is a list of rasters for individual each within a given
 #'  interval time frame.
-
-
-
+#'
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#' @importFrom raster raster rasterize values cellStats
 
 #' @export
-
 iso2raster <- function(isopleth, resolution) {
   # Creates empty list
   template_raster <- vector("list", length(isopleth))
@@ -27,22 +26,22 @@ iso2raster <- function(isopleth, resolution) {
     setTxtProgressBar(pb, i)
 
     # Creates template raster
-    template_raster[[i]] <- raster::raster(isopleth[[i]],
+    template_raster[[i]] <- raster(isopleth[[i]],
       resolution = resolution, vals = 0,
       crs = sp::CRS(sf::st_crs(isopleth[[i]])[[2]])
     )
     # Rasterizes the isopleth
-    raster_locoh[[i]] <- raster::rasterize(isopleth[[i]],
+    raster_locoh[[i]] <- rasterize(isopleth[[i]],
       template_raster[[i]],
       field = "level",
       fun = "first"
     )
 
     # Now you want to subtract all the isopleth values from 1
-    raster::values(raster_locoh[[i]]) <- 1 - raster::values(raster_locoh[[i]])
+    values(raster_locoh[[i]]) <- 1 - values(raster_locoh[[i]])
 
     # Now normalize
-    x[[i]] <- raster_locoh[[i]] / raster::cellStats(raster_locoh[[i]], sum)
+    x[[i]] <- raster_locoh[[i]] / cellStats(raster_locoh[[i]], sum)
   }
   return(x)
   close(pb)
