2a447e5b83373995292e349f10b70b518832e700
John Huang <john.huang5595@gmail.com>
2022-04-17 15:10:14 -0600

    Update

--- R/match_interval_fun.R
+++ R/match_interval_fun.R
@@ -3,14 +3,13 @@
 #' @param list1 A list object that is the reference for `list2`
 #' @param list2 A list object that the length of `list1` is being matched to
 #'
-#' @return The output is a list that has the same intervals as that in the 
+#' @return The output is a list that has the same intervals as that in the
 #' `list2` object.
 
 #' @export
-#' 
 match_intervals <- function(list1, list2){
-i1 <- sub("(.*)-\\d+-(.*)", "\\1-\\2", 
-          names(list1)) %in% sub("(.*)-\\d+-(.*)", 
+i1 <- sub("(.*)-\\d+-(.*)", "\\1-\\2",
+          names(list1)) %in% sub("(.*)-\\d+-(.*)",
                                 "\\1-\\2", names(list2))
 list1 <- list1[i1]
 
